ef2ebc04570401bb6977692c8acc630c00e3b5a4
DuongChi <chidqph04472@fpt.edu.vn>
2016-11-15 00:59:21 +0700

    Chí

--- app/src/main/java/com/example/team2_mobilephim/team2_mobilephiem/Activity_Content.java
+++ app/src/main/java/com/example/team2_mobilephim/team2_mobilephiem/Activity_Content.java
@@ -3,11 +3,13 @@ package com.example.team2_mobilephim.team2_mobilephiem;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.MediaController;
 import android.widget.TabHost;
 import android.widget.VideoView;
 
-public class Activity_Content extends AppCompatActivity {
+public class Activity_Content extends AppCompatActivity  {
     TabHost tabhost;
     VideoView vview;
 
@@ -15,6 +17,7 @@ public class Activity_Content extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_content);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         TabHost host = (TabHost) findViewById(R.id.tabHost);
         try {
             String link = getIntent().getStringExtra("urls");
@@ -40,5 +43,21 @@ public class Activity_Content extends AppCompatActivity {
         tab1.setContent(R.id.tab2);
         tab1.setIndicator("Server");
         host.addTab(tab1);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.home) {
+            this.finish();
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
